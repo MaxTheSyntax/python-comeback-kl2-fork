11743f6e68541eb12c17ec4235d06a099355e62b
MaxTheSyntax <117991235+MaxTheSyntax@users.noreply.github.com>
2024-01-17 07:26:39 +0000

    zoptymalizowanie kodu

--- calki-chyba.py
+++ calki-chyba.py
@@ -1,13 +1,10 @@
 #Sx^ndx = x^(n+1) / n+1
 
-def f(x):
-    return x+3
-
 def prostokaty(a,b,n):
     dx = (b-a)/n
     s = 0
     for i in range(n):
-        s += f(a+i * dx) *dx
+        s += (a + i * dx + 3) * dx
     return s
 
-print(prostokaty(2,6,4000000000))
+print(prostokaty(2,6,40))
